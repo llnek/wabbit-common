ac4b08194127854e7eabdee442c0057c32398d88
Kenneth Leung <ccllnekk@gmail.com>
2017-01-23 01:21:30 -0800

    kl

--- src/main/resources/czlab/wabbit/shared/new/project.clj
+++ src/main/resources/czlab/wabbit/shared/new/project.clj
@@ -9,6 +9,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [io.czlab/wabbit-cons "1.0.0"]
                  [io.czlab/wabbit "1.0.0"]]
 
   :plugins [[lein-codox "0.10.2"]
