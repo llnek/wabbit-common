4bbd7307671efd5c9a8090d5bc4f32f123332e12
Kenneth Leung <ccllnekk@gmail.com>
2017-01-27 03:04:24 -0800

    kl

--- src/main/resources/czlab/wabbit/shared/new/project.clj
+++ src/main/resources/czlab/wabbit/shared/new/project.clj
@@ -8,43 +8,44 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [io.czlab/wabbit-cons "1.0.0"]
-                 [io.czlab/wabbit "1.0.0"]]
+  :dependencies
+  [[org.clojure/clojurescript "1.9.293"]
+   [org.clojure/clojure "1.8.0"]
+   [net.mikera/cljunit "0.6.0" :scope "test"]
+   [junit/junit "4.12" :scope "test"]
+   [io.czlab/wabbit-cons "1.0.0"]
+   [io.czlab/wabbit "1.0.0"]]
 
-  :plugins [[lein-codox "0.10.2"]
+  :plugins [[wabbit/lein-template "1.0.0"]
+            [lein-codox "0.10.2"]
             [lein-pprint "1.1.2"]]
 
-  :profiles {:provided {:dependencies
-                        [[org.clojure/clojurescript "1.9.293" :scope "provided"]
-                         [org.clojure/clojure "1.8.0" :scope "provided"]
-                         [net.mikera/cljunit "0.6.0" :scope "test"]
-                         [junit/junit "4.12" :scope "test"]
-                         [codox/codox "0.10.2" :scope "provided"]]}
-             :uberjar {:aot :all}}
+  :kill-port "localhost:4444"
+  :profiles
+  {:podify
+   {:agentlib "-agentlib:jdwp=transport=dt_socket,server=y,address=8787,suspend=n"
+    :jvm-opts ^:replace
+    ["-XX:+CMSClassUnloadingEnabled"
+     "-XX:+UseConcMarkSweepGC"
+     "-Xms1g"
+     "-Xmx8g"
+     "-Dwabbit.kill.port=@@kill-port@@"
+     "-Dlog4j.configurationFile=file:etc/log4j2d.xml"]}
+   :uberjar {:aot :all}}
 
   :global-vars {*warn-on-reflection* true}
   :target-path "target/%s"
   :aot :all
 
+  :aliases {"deploy" ["with-profile"
+                      "podify" "wabbit"]
+            "run" ["trampoline"
+                   "run" "-m" "czlab.wabbit.sys.core"]}
+
   :java-source-paths ["src/main/java" "src/test/java"]
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
   :resource-paths ["src/main/resources"]
-
-  :debugger
-  {:profiler ""
-   :agent "-agentlib:jdwp=transport=dt_socket,server=y,address=8787,suspend=n"}
-
-  :runtime
-  {:kill-port "localhost:4444"
-   :jvm-opts
-   ["-XX:+CMSClassUnloadingEnabled"
-    "-XX:+UseConcMarkSweepGC"
-    "-Xms1g"
-    "-Xmx8g"
-    "-Dwabbit.kill.port={{kill-port}}"
-    "-Dlog4j.configurationFile=file:etc/log4j2d.xml"]}
 
   :jvm-opts ["-Dlog4j.configurationFile=file:etc/log4j2c.xml"]
   :javac-options ["-source" "8"
